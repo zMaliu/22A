4f71cb269a1617af5988d326392716bd95bd249c
zMaliu <znaihe@hotmail.com>
2025-08-29 11:33:09 +0800

    修改

--- makeParam.m
+++ makeParam.m
@@ -25,41 +25,14 @@ param.L0 = 0.5;        % m    弹簧原长（垂向）
 % 5. 静水恢复力矩系数
 param.C44 = 8890.7;    % N·m  纵摇静水恢复力矩系数
 
-% 6. 按问题号分频插值
-% ——— 问题 1 ———
-param.q1.omega = 1.4005;
-param.q1.A33   = 1335.535;
-param.q1.B33   = 656.3616;
-param.q1.f     = 6250;
-param.q1.A55   = 6779.315;   % kg·m^2
-param.q1.B55   = 151.4388;
-param.q1.L     = 1230;
+% 6. 根据"4Q_param"对应填充不同问题的参数
+param.q1.B = 1.4005;
+param.q1.C = 1335.535;
+param.q1.D = 656.3616;
+param.q1.E = 6250;
+param.q1.F = 6779.315;   % kg·m^2
+param.q1.G = 151.4388;
+param.q1.H = 1230;
 
-% ——— 问题 2 ———
-param.q2.omega = 2.2143;
-param.q2.A33   = 1165.992;
-param.q2.B33   = 167.8395;
-param.q2.f     = 4890;
-param.q2.A55   = 7131.29;
-param.q2.B55   = 2992.724;
-param.q2.L     = 2560;
-
-% ——— 问题 3 ———
-param.q3.omega = 1.7152;
-param.q3.A33   = 1028.876;
-param.q3.B33   = 683.4558;
-param.q3.f     = 3640;
-param.q3.A55   = 7001.914;
-param.q3.B55   = 654.3383;
-param.q3.L     = 1690;
-
-% ——— 问题 4 ———
-param.q4.omega = 1.9806;
-param.q4.A33   = 1091.099;
-param.q4.B33   = 528.5018;
-param.q4.f     = 1760;
-param.q4.A55   = 7142.493;
-param.q4.B55   = 1655.909;
-param.q4.L     = 2140;
 
 end
